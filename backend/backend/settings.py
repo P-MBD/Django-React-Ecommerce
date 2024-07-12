c81128843b2e3be78b14db66c248f92f4f5706b1
P-MBD <112797204+P-MBD@users.noreply.github.com>
2024-07-12 09:34:35 +0330

    2. Custom Django Admin using Jazzmin Package

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -127,3 +128,24 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    # title of the window (Will default to current_admin_site.site_title if absent or None)
+    "site_title": "Desphix",
+
+    # Title on the login screen (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_header": "Desphix",
+
+    # Title on the brand (19 chars max) (defaults to current_admin_site.site_header if absent or None)
+    "site_brand": "Desphix",
+
+     "welcome_sign": "Welcome to Desphix",
+
+     # Copyright on the footer
+    "copyright": "Desphix 2024 Alright Reserved",
+
+     # Whether to display the side menu
+    "show_sidebar": True,
+    "show_ui_builder": True,
+
+}
